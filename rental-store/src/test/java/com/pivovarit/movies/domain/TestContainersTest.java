755d32ce5c5ae337e93bdcf1ce3074ed37e0d20f
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2025-08-20 13:39:13 +0200

    testcontainers

---
+++ rental-store/src/test/java/com/pivovarit/movies/domain/TestContainersTest.java
@@ -0,0 +1,25 @@
+package com.pivovarit.movies.domain;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestContainersTest {
+
+    @Test
+    void testDockerIsWorkingWithBusyBox() throws Exception {
+        try (GenericContainer<?> busybox = new GenericContainer<>(DockerImageName.parse("busybox:1.36"))
+          .withCommand("sleep", "5")) { // keep container alive briefly
+            busybox.start();
+
+            String output = busybox
+              .execInContainer("echo", "hello-busybox")
+              .getStdout()
+              .trim();
+
+            assertEquals("hello-busybox", output);
+        }
+    }
+}
