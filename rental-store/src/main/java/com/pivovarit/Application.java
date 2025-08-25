0dbe2c13a703856171fa31931524421620df2e41
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2025-08-25 16:13:12 +0200

    7. movie descriptions - integration

--- rental-store/src/main/java/com/pivovarit/Application.java
+++ rental-store/src/main/java/com/pivovarit/Application.java
@@ -3,6 +3,8 @@ package com.pivovarit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 1 rozbudować RentalFacade#findById/findByTitle: zwracanie dodatkowo opisu filmu
+// 2 dodać interface DescriptionsRepository#findById
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
--- rental-store/src/main/java/com/pivovarit/web/MoviesController.java
+++ rental-store/src/main/java/com/pivovarit/web/MoviesController.java
@@ -1,8 +1,11 @@
 package com.pivovarit.web;
 
 import com.pivovarit.domain.rental.RentalFacade;
+import com.pivovarit.domain.rental.api.MovieAddRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
@@ -20,6 +23,12 @@ public class MoviesController {
     public Optional<MovieResponse> getMovieById(@PathVariable int id) {
         return movieFacade.findById(id)
           .map(m -> new MovieResponse(m.id().id(), m.title(), m.type().toString()));
+    }
+
+    // curl -X POST -H "Content-Type: application/json" -d '{"id": 1, "title": "The Matrix", "type": "NEW"}' http://localhost:8081/movies
+    @PostMapping("/movies")
+    public void addMovie(@RequestBody MovieAddRequest movie) {
+        movieFacade.addMovie(movie);
     }
 
     public record MovieResponse(long id, String title, String type) {
